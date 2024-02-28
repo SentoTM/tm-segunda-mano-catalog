69c25f7adc97b7f8b7ce05f3db44dc97dd008b6b
Vicente Tormo <vicent.tormo@gmail.com>
2024-02-28 16:16:56 +0100

    Vue app created and components first version

---
+++ tm-sm-catalogue/src/components/FiltrosComponente.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="filtros">
+    <h2> Filtros:</h2>
+    <select v-model="selectedCategoria">
+      <option value="">Todas las categorias</option>
+      <option v-for="categoria in categorias" :key="categoria">{{ categoria}}</option>
+    </select>
+    <input type="number" v-model.number="precioMin" placeholder="Precio mínimo">
+    <input type="number" v-model.number="precioMax" placeholder="Precio máximo">
+    <button @click="aplicarFiltros">Aplicar filtros</button>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      selectedCategoria: '',
+      precioMin: null,
+      precioMax: null
+    };   
+  },
+  props: ['categorias'],
+  methods: {
+    aplicarFiltros() {
+      this.$emit('filtrar', {categoria: this.selectedCategoria, precioMin: this.precioMin, precioMax: this.precioMax });
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .filtros {
+    margin-bottom: 20px;
+  }
+</style>
---
+++ tm-sm-catalogue/src/components/ObjetoComponente.vue
@@ -0,0 +1,21 @@
+<template>
+  <article class="objeto">
+    <img :src="objeto.imagen" alt="objeto.descripcion">
+    <h3>{{ objeto.nombre }}</h3>
+    <p>Categoría: {{ objeto.categoria }}</p>
+    <p>Precio: {{ objeto.precio }}</p>
+    <p>{{ objeto.descripcion }}</p>
+  </article>
+</template>
+
+<script>
+  export default {
+    props: [objeto]
+  }
+</script>
+
+<style scoped>
+  .objeto {
+    margin-bottom: 20px;
+  }
+</style>
